6dd4931bd8cf9ac53f23e9d27f8f1812df61bbe5
Thomas <Thomasnathanct@gmail.com>
2018-01-27 19:38:43 +0000

    Upload special effects

--- Bat/Assets/Scripts/Mover.cs
+++ Bat/Assets/Scripts/Mover.cs
@@ -6,10 +6,14 @@ public class Mover : MonoBehaviour {
 	public float speed;
     Rigidbody RB;
     public float rotatespeed = 50f;
+    public GameObject Explosion;
+    bool hit;
+    public float time = 1;
 
 	void Awake() {
         RB = GetComponentInChildren<Rigidbody>();
 		RB.velocity = transform.forward * speed;
+        Explosion.SetActive(false);
 	}
 
 
@@ -18,9 +22,20 @@ public class Mover : MonoBehaviour {
         float turn = speed * Time.deltaTime;
         Quaternion turnRotation = Quaternion.Euler(rotatespeed, 0f, 0f);
         RB.MoveRotation(RB.rotation * turnRotation);
+
+        if(hit == true)
+        {
+            time -= Time.deltaTime;
+            Explosion.SetActive(true);
+        }
+        if (time <= 0){
+            Destroy(gameObject);
+        }
     }
 
-    
-
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
 
 }
