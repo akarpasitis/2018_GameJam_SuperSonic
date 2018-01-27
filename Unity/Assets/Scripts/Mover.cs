21334c9f1ff812e83058eb60ee22b5a0ef373690
Thomas Nathan CT <32567705+TNathanCT@users.noreply.github.com>
2018-01-27 16:36:40 +0000

    Update Mover.cs

--- Unity/Assets/Scripts/Mover.cs
+++ Unity/Assets/Scripts/Mover.cs
@@ -1,13 +1,24 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Mover : MonoBehaviour {
-
-	public float speed;
+	
+    public float speed;
     Rigidbody RB;
+    public float rotatespeed = 50f;
 
-	void Start() {
+    void Start() {
         RB = GetComponent<Rigidbody>();
-		RB.velocity = transform.forward * speed;
-	}
+	RB.velocity = transform.forward * speed;
+    }
+
+
+    private void FixedUpdate()
+    {
+        float turn = speed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(rotatespeed, 0f, 0f);
+        RB.MoveRotation(RB.rotation * turnRotation);
+    }
+
+
 }
